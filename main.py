922067f27d101b11a83ca12906e45f416d7717c4
Devin Johnson <j00847797@students.jsums.edu>
2023-09-09 16:41:34 -0500

    Slot Machine System Progress

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+# 3 wheels
+ROW = 3
+# The wheels themselves.
+# Basically a column full of symbols and each symbol appears a number of times in the column.
+COL = 3
+
+# Dictionary for the symbols found in the wheels/columns and their frequency
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
 
 # Collect deposit from user
 # isdigit() checks if strings are positive integers. It does not 
@@ -36,7 +52,7 @@ def get_num_of_lines():
 
 def get_bet():
     while True:
-        bet = input("Please enter the amount you want to bet: $")
+        bet = input("What would you like to bet on each line? $")
         if bet.isdigit():
             bet = int(bet)
             if MIN_BET <= bet <= MAX_BET:
@@ -48,6 +64,45 @@ def get_bet():
 
     return bet
 
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    # .items() gets the key and key value from the dictionary
+    # This allows us to get both values without having to manually reference them
+    for symbol, symbol_count in symbols.items():
+        # we don't actually care about the elements so we can just use an _ to cycle through the range.
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    # Create a list of columns
+    columns = []
+    # For every column we need to generate a number of symbols
+    for _ in range(cols):
+        column = []
+        # create a copy of all symbols
+        # It won't be possible to use the entire symbol count to fill all 3 columns because 
+        # this is a 3x3 matrix. There are only 9 cells to fill
+        # So, we want to randomly fill a value from the dictionary in the column
+        # and then romove that item from the list of current_symbol until the column is complete
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, col in enumerate(columns):
+            if i != len(columns) - 1:
+                print(col[row], end=" | ")
+            else:
+                print(col[row], end="")
+
+        print()
 
 
 # this main function is not like C++
@@ -55,9 +110,23 @@ def get_bet():
 # this function will just make it easier to allow the player to play the game again
 def main(): 
     balance = deposit()
+    print(f"Your balance is: ${balance}")
     lines = get_num_of_lines()
-    bet = get_bet()
 
+    # get bet from user and find total bet
+    # check if total bet <= balance
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(f"Bet cannot be more than balance available! Current balance is: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROW, COL, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
