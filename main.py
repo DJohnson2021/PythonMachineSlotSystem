665b6c138f77ed8d520de3b9e51fb110a6c1b00d
Devin Johnson <j00847797@students.jsums.edu>
2023-09-09 23:22:20 -0500

    Finsished slot machine game

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ symbol_count = {
     "D": 8,
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2,
+}
+
 # Collect deposit from user
 # isdigit() checks if strings are positive integers. It does not 
 def deposit():
@@ -49,7 +56,6 @@ def get_num_of_lines():
     
     return lines
 
-
 def get_bet():
     while True:
         bet = input("What would you like to bet on each line? $")
@@ -64,6 +70,32 @@ def get_bet():
 
     return bet
 
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winnings_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            # calculate winnings
+            winnings += values[symbol] * bet
+            # What line user won on
+            winnings_lines.append(line + 1)
+    
+    return winnings, winnings_lines 
+
+
+# I don't like the logic used here to create the slots for the machine.
+# I basically made an lists of lists to represent the columns of the slot machine
+# But this approach is logically confusing
+# Whenever I use brackets double brackets, I think I'm referring to the rows and columns like i and j
+# But, no that is not the case. I am only referring to the cell
+# in another list within the list of columns
+# I honestly think just using a 2D array would've been better. 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     # .items() gets the key and key value from the dictionary
@@ -105,12 +137,7 @@ def print_slot_machine(columns):
         print()
 
 
-# this main function is not like C++
-# Python is a script based language. It doesn't have a "main" function
-# this function will just make it easier to allow the player to play the game again
-def main(): 
-    balance = deposit()
-    print(f"Your balance is: ${balance}")
+def spin(balance):
     lines = get_num_of_lines()
 
     # get bet from user and find total bet
@@ -127,6 +154,29 @@ def main():
 
     slots = get_slot_machine_spin(ROW, COL, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    if winnings == 0:
+        print(f"You have won on 0 lines.")
+    else:   
+        print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
 
+
+# this main function is not like C++
+# Python is a script based language. It doesn't have a "main" function
+# this function will just make it easier to allow the player to play the game again
+def main(): 
+    balance = deposit()
+    while balance > 0:
+        print(f"Current balance is ${balance}")
+        user_answer = input("Press enter to play (q to quit).")
+        if user_answer == "q":
+            break
+        balance += spin(balance)
+        if balance <= 0:
+            break
+
+    print(f"You're left with ${balance}")
 
 main()
